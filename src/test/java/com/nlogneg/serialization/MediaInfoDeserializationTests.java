4bdfc9b27a235f4ea7fc1bd5f8c75fe8287efd83
Andrew <helios2k6@gmail.com>
2014-02-18 22:54:45 +0000

    more updates to the pom and tests

--- src/test/java/com/nlogneg/serialization/MediaInfoDeserializationTests.java
+++ src/test/java/com/nlogneg/serialization/MediaInfoDeserializationTests.java
@@ -1,15 +1,13 @@
 package com.nlogneg.serialization;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import static org.junit.Assert.assertNotNull;
 
-@RunWith(JUnit4.class)
+import org.junit.Test;
+
 public class MediaInfoDeserializationTests{
 	
 	@Test
 	public void testDeserializationOfVideoAudioTextTrack(){
-		throw new NullPointerException();
-		//assertNotNull("Test file missing. Cannot perform test.", getClass().getResource("/media_info_v_0_7_67.xml"));
+		assertNotNull("Test file missing. Cannot perform test.", getClass().getResource("/media_info_v_0_7_67.xml"));
 	}
 }
