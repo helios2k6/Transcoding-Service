7112dfd86c85dea2bbb0daa8c0ad2aa9f9f3ee79
Andrew <helios2k6@gmail.com>
2014-03-12 21:52:18 +0100

    added some documents and the gitignore in prep for the C#/F# rewrite

---
+++ src/Media Information Service/Media Information Service/Driver.cs
@@ -0,0 +1,16 @@
+﻿namespace Media_Information_Service
+{
+   /// <summary>
+   /// The main entry point for this console application
+   /// </summary>
+   public static class Driver
+   {
+      /// <summary>
+      /// The main function
+      /// </summary>
+      /// <param name="args">Program arguments</param>
+      public static void Main(string[] args)
+      {
+      }
+   }
+}
